ff3a2c71dceebed4c1a7248f48e1a841d05c7e49
Daniel Chung <danielchung@Daniels-MacBook-Pro.local>
2023-01-02 00:20:28 -0500

    adding total stats route

--- app.js
+++ app.js
@@ -15,13 +15,38 @@ app.get('/', (req, res) => {
 app.get('/testdb', (req, res) => {
 
 	conn.query('SELECT * FROM matchhistory', (error, results, fields) => {
-		if (error){
-			throw error
-		}
+		if (error) throw error
 		res.send(results);
 	});
-
 })
+
+app.get('/totalstats', (req, res) => {
+	let wins;
+	let loss;
+	conn.query('SELECT (SELECT COUNT(*) FROM matchhistory WHERE result = 1) as wins, (SELECT COUNT(*) FROM matchhistory WHERE result = 2) as loss', (error, results, fields) => {
+		if (error) throw error
+		let data = results[0]
+		wins = data.wins
+		loss = data.loss
+		total = wins + loss
+		winrate = (wins * 100 / total).toFixed(0)
+		let returnObject = {
+			'wins': wins.toString(),
+			'loss': loss.toString(),
+			'totalgames': total.toString(),
+			'winrate': winrate.toString()
+		}
+		res.send(returnObject)
+	})
+})
+
+// app.post('/insertgame', (req, res) => {
+
+// 	conn.query ('',(error, results, fields) => {
+
+// 	})
+// 	res.send()
+// })
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
