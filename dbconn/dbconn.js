80b45995d90a9356dc1a5181c28942034c6ea8b2
Daniel Chung <danielchung@Daniels-MacBook-Pro.local>
2023-01-01 21:55:58 -0500

    testing changes

--- dbconn/dbconn.js
+++ dbconn/dbconn.js
@@ -1,20 +1,22 @@
 const mysql = require('mysql2')
 
-// const conn = mysql.createConnection({
-// 	host: "az-db.c2irzkybqtvy.us-east-1.rds.amazonaws.com",
-// 	user: "root",
-// 	password: "testpassword",
-// 	port: 3306,
-// 	database: 'azfill'
-// })
-
 const conn = mysql.createConnection({
-	host: process.env.HOST,
-	user: process.env.USER,
-	password: process.env.PASSWORD,
-	port: process.env.PORT,
-	database: process.env.DATABASE
+	host: "az-db.c2irzkybqtvy.us-east-1.rds.amazonaws.com",
+	user: "root",
+	password: "testpassword",
+	port: 3306,
+	database: 'azfill'
 })
+
+console.log(process.env.USER)
+
+// const conn = mysql.createConnection({
+// 	host: process.env.HOST,
+// 	user: process.env.USER,
+// 	password: process.env.PASSWORD,
+// 	port: process.env.PORT,
+// 	database: process.env.DATABASE
+// })
 
 conn.connect()
   
