a6b7f1a449e2790ba066a049cfee2c6448678839
Daniel Chung <pikachungg@gmail.com>
2023-01-06 17:40:35 -0500

    cjamges

--- app.js
+++ app.js
@@ -16,54 +16,54 @@ app.get('/', (req, res) => {
   res.send('A to Z api')
 })
 
-app.get('/testdb', (req, res) => {
+// app.get('/testdb', (req, res) => {
 
-	pool.query('SELECT * FROM matchhistory', (error, results, fields) => {
-		if (error) throw error
-		res.send(results);
-	});
-})
+// 	pool.query('SELECT * FROM matchhistory', (error, results, fields) => {
+// 		if (error) throw error
+// 		res.send(results);
+// 	});
+// })
 
-app.get('/totalstats', (req, res) => {
-	let wins;
-	let loss;
-	pool.query(`SELECT (SELECT COUNT(*) FROM matchhistory WHERE result = 1) as wins, (SELECT COUNT(*) FROM matchhistory WHERE result = 2) as loss`, (error, results, fields) => {
-		if (error) throw error
-		let data = results[0]
-		wins = data.wins
-		loss = data.loss
-		total = wins + loss
-		winrate = (wins * 100 / total).toFixed(0)
-		let returnObject = {
-			'wins': wins.toString(),
-			'loss': loss.toString(),
-			'totalgames': total.toString(),
-			'winrate': winrate.toString()
-		}
-		res.send(returnObject)
-	})
-})
+// app.get('/totalstats', (req, res) => {
+// 	let wins;
+// 	let loss;
+// 	pool.query(`SELECT (SELECT COUNT(*) FROM matchhistory WHERE result = 1) as wins, (SELECT COUNT(*) FROM matchhistory WHERE result = 2) as loss`, (error, results, fields) => {
+// 		if (error) throw error
+// 		let data = results[0]
+// 		wins = data.wins
+// 		loss = data.loss
+// 		total = wins + loss
+// 		winrate = (wins * 100 / total).toFixed(0)
+// 		let returnObject = {
+// 			'wins': wins.toString(),
+// 			'loss': loss.toString(),
+// 			'totalgames': total.toString(),
+// 			'winrate': winrate.toString()
+// 		}
+// 		res.send(returnObject)
+// 	})
+// })
 
-app.get('/mostplayedposition', (req, res) => {
-	pool.query(`SELECT position, COUNT(position) as 'count' FROM matchhistory GROUP BY position ORDER BY 'count' DESC LIMIT 1`, (error, results, fields) => {
-		if (error) throw error
-		let data = results[0]
-		let returnObject = {
-			'position': numberToPosition(data.position),
-			'amountplayed': data.count
-		}
-		res.send(returnObject)
-	})
-})
+// app.get('/mostplayedposition', (req, res) => {
+// 	pool.query(`SELECT position, COUNT(position) as 'count' FROM matchhistory GROUP BY position ORDER BY 'count' DESC LIMIT 1`, (error, results, fields) => {
+// 		if (error) throw error
+// 		let data = results[0]
+// 		let returnObject = {
+// 			'position': numberToPosition(data.position),
+// 			'amountplayed': data.count
+// 		}
+// 		res.send(returnObject)
+// 	})
+// })
 
-app.get('/champion/:championid', (req, res) => {
-	let {championid} = req.query.championid
-	pool.query(`SELECT * FROM matchhistory WHERE champion = ${championid}`, (error, results, fields) => {
-    if (error) throw error
-    let data = results
-    res.send(data)
-	})
-})
+// app.get('/champion/:championid', (req, res) => {
+// 	let {championid} = req.query.championid
+// 	pool.query(`SELECT * FROM matchhistory WHERE champion = ${championid}`, (error, results, fields) => {
+//     if (error) throw error
+//     let data = results
+//     res.send(data)
+// 	})
+// })
 
 // app.post('/insertgame', async (req, res) => {
 // 	const { matchid, auth } = req.body
