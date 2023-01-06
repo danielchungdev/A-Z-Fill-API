2a29c1ebdbd48d4b42300f5eb55f3846247a605c
Daniel Chung <pikachungg@gmail.com>
2023-01-06 17:26:46 -0500

    changing

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const PORT = process.env.PORT || 8081
 const cors = require('cors');
 const bodyParser = require('body-parser'); 
 const pool = require('./dbconn/dbconn')
-const positionSwitch = require('./functions/positionswitch')
+const numberToPosition = require('./functions/numberToPosition')
+const positionToNumber = require('./functions/positionToNumber')
+const axios = require('axios')
+const determineResult = require('./functions/determineResult')
 
 app.use(cors());
 app.use(bodyParser.json()); 
@@ -46,7 +49,7 @@ app.get('/mostplayedposition', (req, res) => {
 		if (error) throw error
 		let data = results[0]
 		let returnObject = {
-			'position': positionSwitch(data.position),
+			'position': numberToPosition(data.position),
 			'amountplayed': data.count
 		}
 		res.send(returnObject)
@@ -54,20 +57,48 @@ app.get('/mostplayedposition', (req, res) => {
 })
 
 app.get('/champion/:championid', (req, res) => {
-	let championid = req.params.championid
-	pool.query(`SELECT * FROM matchhistory WHERE champion = '${championid}'`, (error, results, fields) => {
+	let {championid} = req.query.championid
+	pool.query(`SELECT * FROM matchhistory WHERE champion = ${championid}`, (error, results, fields) => {
     if (error) throw error
     let data = results
     res.send(data)
 	})
 })
 
-app.post('/insertgame', (req, res) => {
+app.post('/insertgame', async (req, res) => {
+	const { matchid, auth } = req.body
 
-	conn.query (`INSERT INTO matchhistory (matchid, champion)`,(error, results, fields) => {
-		
+	const counterAuth = process.env.AUTH
+
+	if (auth !== "130posnrJAO$2945"){
+		return res.send({"message": "You're not authorize to use this API route"})
+	}
+
+	const apikey = "RGAPI-06cf436b-3a3f-467d-bdc8-69016b3df391"
+
+	const { championName, individualPosition, kills, deaths, assists, win } = await axios.get(`https://americas.api.riotgames.com/lol/match/v5/matches/NA1_${matchid}?api_key=${apikey}`)
+		.then( response =>  {
+			response = response.data.info.participants
+			let user;
+			for (let i = 0; i < response.length; i++){
+				if ( response[i].summonerName === 'Shapeless'){
+					user = response[i]
+					break;
+				}
+			}
+			return user
+		})
+		.catch( (error) => {
+			console.log(error)
+		})
+
+	pool.query (`INSERT INTO matchhistory (matchid, champion, result, position, kills, deaths, assists, screenshot) VALUES (${matchid}, '${championName}', ${determineResult(win)}, ${positionToNumber(individualPosition)}, ${kills}, ${deaths}, ${assists}, 'screenshot.aws.com')`,(error, results, fields) => {
+		if (error) throw error
+		let data = results
+		console.log(data)
 	})
-	res.send({})
+
+	res.send({message: `Inserted match with id: ${matchid}`})
 })
 
 app.listen(PORT, () => {
