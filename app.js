69b9be16ade22e66c786826611187e89e439bdf0
Daniel Chung <pikachungg@gmail.com>
2023-01-06 19:43:55 -0500

    ajkshdfaksjhdf

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.get('/testdb', (req, res) => {
 	// 	if (error) throw error
 	// 	res.send(results);
 	// });
-	connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+	pool.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
 		if (err) throw err
 		console.log('The solution is: ', rows[0].solution)
 	})
