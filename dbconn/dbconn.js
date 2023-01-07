8329df358aed3c0bad99444ef78f12c6abbc0c10
Daniel Chung <pikachungg@gmail.com>
2023-01-06 19:38:44 -0500

    changing to env

--- dbconn/dbconn.js
+++ dbconn/dbconn.js
@@ -1,19 +1,19 @@
 const mysql = require('mysql2')
 
-const pool = mysql.createPool({
-	host: "az-db.c2irzkybqtvy.us-east-1.rds.amazonaws.com",
-	user: "root",
-	password: "testpassword",
-	port: 3306,
-	database: 'azfill'
-})
-
-// const conn = mysql.createConnection({
-// 	host: process.env.HOST,
-// 	user: process.env.USER,
-// 	password: process.env.PASSWORD,
-// 	port: process.env.PORT,
-// 	database: process.env.DATABASE
+// const pool = mysql.createPool({
+// 	host: "az-db.c2irzkybqtvy.us-east-1.rds.amazonaws.com",
+// 	user: "root",
+// 	password: "testpassword",
+// 	port: 3306,
+// 	database: 'azfill'
 // })
+
+const pool = mysql.createPool({
+	host: process.env.HOST,
+	user: process.env.USER,
+	password: process.env.PASSWORD,
+	port: process.env.PORT,
+	database: process.env.DATABASE
+})
   
 module.exports = pool 
