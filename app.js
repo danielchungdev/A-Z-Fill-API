2639f8ab0aa8ab181307f125510315dee06a8f8f
Daniel Chung <pikachungg@gmail.com>
2023-01-06 19:58:18 -0500

    restored

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const app = express()
 const PORT = process.env.PORT || 8081
 const cors = require('cors');
 const bodyParser = require('body-parser'); 
-const conn = require('./dbconn/dbconn')
-
+const pool = require('./dbconn/dbconn')
+const numberToPosition = require('./functions/numberToPosition')
+const positionToNumber = require('./functions/positionToNumber')
+const axios = require('axios')
+const determineResult = require('./functions/determineResult')
 
 app.use(cors());
 app.use(bodyParser.json()); 
@@ -15,91 +18,88 @@ app.get('/', (req, res) => {
 
 app.get('/testdb', (req, res) => {
 
-	// pool.query('SELECT * FROM matchhistory', (error, results, fields) => {
-	// 	if (error) throw error
-	// 	res.send(results);
-	// });
-	conn.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
-		if (err) throw err
-		console.log('The solution is: ', rows[0].solution)
+	pool.query('SELECT * FROM matchhistory', (error, results, fields) => {
+		if (error) throw error
+		res.send(results);
+	});
+})
+
+app.get('/totalstats', (req, res) => {
+	let wins;
+	let loss;
+	pool.query(`SELECT (SELECT COUNT(*) FROM matchhistory WHERE result = 1) as wins, (SELECT COUNT(*) FROM matchhistory WHERE result = 2) as loss`, (error, results, fields) => {
+		if (error) throw error
+		let data = results[0]
+		wins = data.wins
+		loss = data.loss
+		total = wins + loss
+		winrate = (wins * 100 / total).toFixed(0)
+		let returnObject = {
+			'wins': wins.toString(),
+			'loss': loss.toString(),
+			'totalgames': total.toString(),
+			'winrate': winrate.toString()
+		}
+		res.send(returnObject)
 	})
 })
 
-// app.get('/totalstats', (req, res) => {
-// 	let wins;
-// 	let loss;
-// 	pool.query(`SELECT (SELECT COUNT(*) FROM matchhistory WHERE result = 1) as wins, (SELECT COUNT(*) FROM matchhistory WHERE result = 2) as loss`, (error, results, fields) => {
-// 		if (error) throw error
-// 		let data = results[0]
-// 		wins = data.wins
-// 		loss = data.loss
-// 		total = wins + loss
-// 		winrate = (wins * 100 / total).toFixed(0)
-// 		let returnObject = {
-// 			'wins': wins.toString(),
-// 			'loss': loss.toString(),
-// 			'totalgames': total.toString(),
-// 			'winrate': winrate.toString()
-// 		}
-// 		res.send(returnObject)
-// 	})
-// })
+app.get('/mostplayedposition', (req, res) => {
+	pool.query(`SELECT position, COUNT(position) as 'count' FROM matchhistory GROUP BY position ORDER BY 'count' DESC LIMIT 1`, (error, results, fields) => {
+		if (error) throw error
+		let data = results[0]
+		let returnObject = {
+			'position': numberToPosition(data.position),
+			'amountplayed': data.count
+		}
+		res.send(returnObject)
+	})
+})
 
-// app.get('/mostplayedposition', (req, res) => {
-// 	pool.query(`SELECT position, COUNT(position) as 'count' FROM matchhistory GROUP BY position ORDER BY 'count' DESC LIMIT 1`, (error, results, fields) => {
-// 		if (error) throw error
-// 		let data = results[0]
-// 		let returnObject = {
-// 			'position': numberToPosition(data.position),
-// 			'amountplayed': data.count
-// 		}
-// 		res.send(returnObject)
-// 	})
-// })
+app.get('/champion/:championid', (req, res) => {
+	let {championid} = req.query.championid
+	pool.query(`SELECT * FROM matchhistory WHERE champion = ${championid}`, (error, results, fields) => {
+    if (error) throw error
+    let data = results
+    res.send(data)
+	})
+})
 
-// app.get('/champion/:championid', (req, res) => {
-// 	let {championid} = req.query.championid
-// 	pool.query(`SELECT * FROM matchhistory WHERE champion = ${championid}`, (error, results, fields) => {
-//     if (error) throw error
-//     let data = results
-//     res.send(data)
-// 	})
-// })
+app.post('/insertgame', async (req, res) => {
+	const { matchid, auth } = req.body
 
-// app.post('/insertgame', async (req, res) => {
-// 	const { matchid, auth } = req.body
+	const counterAuth = process.env.AUTH
 
-// 	const counterAuth = process.env.AUTH
+	if (auth !== "130posnrJAO$2945"){
+		return res.send({"message": "You're not authorize to use this API route"})
+	}
 
-// 	if (auth !== "130posnrJAO$2945"){
-// 		return res.send({"message": "You're not authorize to use this API route"})
-// 	}
+	const apikey = "RGAPI-06cf436b-3a3f-467d-bdc8-69016b3df391"
 
+	const { championName, individualPosition, kills, deaths, assists, win } = await axios.get(`https://americas.api.riotgames.com/lol/match/v5/matches/NA1_${matchid}?api_key=${apikey}`)
+		.then( response =>  {
+			response = response.data.info.participants
+			let user;
+			for (let i = 0; i < response.length; i++){
+				if ( response[i].summonerName === 'Shapeless'){
+					user = response[i]
+					break;
+				}
+			}
+			return user
+		})
+		.catch( (error) => {
+			console.log(error)
+		})
 
-// 	const { championName, individualPosition, kills, deaths, assists, win } = await axios.get(`https://americas.api.riotgames.com/lol/match/v5/matches/NA1_${matchid}?api_key=${apikey}`)
-// 		.then( response =>  {
-// 			response = response.data.info.participants
-// 			let user;
-// 			for (let i = 0; i < response.length; i++){
-// 				if ( response[i].summonerName === 'Shapeless'){
-// 					user = response[i]
-// 					break;
-// 				}
-// 			}
-// 			return user
-// 		})
-// 		.catch( (error) => {
-// 			console.log(error)
-// 		})
+	pool.query (`INSERT INTO matchhistory (matchid, champion, result, position, kills, deaths, assists, screenshot) VALUES (${matchid}, '${championName}', ${determineResult(win)}, ${positionToNumber(individualPosition)}, ${kills}, ${deaths}, ${assists}, 'screenshot.aws.com')`,(error, results, fields) => {
+		if (error) throw error
+		let data = results
+		console.log(data)
+	})
 
-// 	pool.query (`INSERT INTO matchhistory (matchid, champion, result, position, kills, deaths, assists, screenshot) VALUES (${matchid}, '${championName}', ${determineResult(win)}, ${positionToNumber(individualPosition)}, ${kills}, ${deaths}, ${assists}, 'screenshot.aws.com')`,(error, results, fields) => {
-// 		if (error) throw error
-// 		let data = results
-// 		console.log(data)
-// 	})
-
-// 	res.send({message: `Inserted match with id: ${matchid}`})
-// })
+	res.send({message: `Inserted match with id: ${matchid}`})
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
---
+++ functions/determineResult.js
@@ -0,0 +1,10 @@
+
+
+const determineResult = (boolean) => {
+	if (boolean){
+		return 1
+	}
+	return 2
+}
+
+module.exports = determineResult
