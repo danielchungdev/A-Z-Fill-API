b968a64060e51b840f7b1410dc10e6dcfa345814
Daniel Chung <pikachungg@gmail.com>
2023-01-09 18:47:32 -0500

    eb update

--- app.js
+++ app.js
@@ -123,5 +123,6 @@ app.post('/insertgame', async (req, res) => {
 })
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
+	console.log("asdhfaskdjfh")
+	console.log(`Example app listening on port ${PORT}`)
 })
