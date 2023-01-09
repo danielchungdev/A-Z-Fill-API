92f5d43a6835837d391775e79ac9cf3dc4329da7
Daniel Chung <pikachungg@gmail.com>
2023-01-08 20:01:13 -0500

    fixing cors issues

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const positionToNumber = require('./functions/positionToNumber')
 const axios = require('axios')
 const determineResult = require('./functions/determineResult')
 
-app.use(cors());
+app.use(cors({origin: '*'}));
 app.use(bodyParser.json()); 
 
 app.get('/', (req, res) => {
